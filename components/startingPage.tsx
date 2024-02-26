8324e5f7de0001779d4c032ea656fd4220411e67
Ivan James Lo <ijalo@addu.edu.ph>
2024-02-26 23:48:43 +0800

    startingPage

---
+++ components/startingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const startingPage = () => {
+  return (
+    <div>startingPage</div>
+  )
+}
+
+export default startingPage
+
+//logo, navbar, and buttons to newEmployees, 
+//assignDesgination, designation, and department
